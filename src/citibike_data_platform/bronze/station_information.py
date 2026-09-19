e70d7aaa6adc7c98abfbe6fd973921ff739a573f
Tom0809 <uyssstom@gmail.com>
2026-09-18 17:45:38 -0700

    autoload bronze

--- src/citibike_data_platform/bronze/station_information.py
+++ src/citibike_data_platform/bronze/station_information.py
@@ -1,17 +1,30 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode, current_timestamp, col
 
+
 spark = SparkSession.builder.getOrCreate()
 
+
 SOURCE_PATH = "s3://tombucket2026/raw/station_information/"
+
+SCHEMA_PATH = "s3://tombucket2026/_schemas/bronze/station_information"
+
+CHECKPOINT_PATH = "s3://tombucket2026/_checkpoints/bronze/station_information"
+
 TARGET_TABLE = "citibike_dev.bronze.station_information"
 
+
 df_raw = (
-    spark.read
-    .option("multiline", "true")
-    .json(SOURCE_PATH)
+    spark.readStream
+    .format("cloudFiles")
+    .option("cloudFiles.format", "json")
+    .option("cloudFiles.schemaLocation", SCHEMA_PATH)
+    .option("cloudFiles.inferColumnTypes", "true")
+    .option("multiLine", "true")
+    .load(SOURCE_PATH)
     .withColumn("_source_file", col("_metadata.file_path"))
 )
+
 
 df_bronze = (
     df_raw
@@ -30,9 +43,14 @@ df_bronze = (
     .withColumn("_ingested_at", current_timestamp())
 )
 
-(
-    df_bronze.write
+
+bronze_stream = (
+    df_bronze.writeStream
     .format("delta")
-    .mode("overwrite")
-    .saveAsTable(TARGET_TABLE)
+    .outputMode("append")
+    .option("checkpointLocation", CHECKPOINT_PATH)
+    .trigger(availableNow=True)
+    .toTable(TARGET_TABLE)
 )
+
+bronze_stream.awaitTermination()
--- src/citibike_data_platform/bronze/station_status.py
+++ src/citibike_data_platform/bronze/station_status.py
@@ -1,35 +1,57 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode, current_timestamp, col
 
+
 spark = SparkSession.builder.getOrCreate()
+
 
 SOURCE_PATH = "s3://tombucket2026/raw/station_status/"
 
+SCHEMA_PATH = "s3://tombucket2026/_schemas/bronze/station_status"
+
+CHECKPOINT_PATH = "s3://tombucket2026/_checkpoints/bronze/station_status"
+
 TARGET_TABLE = "citibike_dev.bronze.station_status"
 
+
+
 df_raw = (
-    spark.read
-    .option("multiline", "true")
-    .json(SOURCE_PATH)
+    spark.readStream
+    .format("cloudFiles")
+    .option("cloudFiles.format", "json")
+    .option("cloudFiles.schemaLocation", SCHEMA_PATH)
+    .option("cloudFiles.inferColumnTypes", "true")
+    .option("multiLine", "true")
+    .load(SOURCE_PATH)
     .withColumn("_source_file", col("_metadata.file_path"))
 )
+
 
 df_bronze = (
     df_raw
     .select(
         explode("data.stations").alias("station"),
         "last_updated",
-        "ttl"
+        "ttl",
+        "_source_file"
     )
     .select(
         "station.*",
         "last_updated",
-        "ttl"
+        "ttl",
+        "_source_file"
     )
     .withColumn("_ingested_at", current_timestamp())
 )
 
-df_bronze.write \
-    .format("delta") \
-    .mode("overwrite") \
-    .saveAsTable(TARGET_TABLE)
+
+bronze_stream = (
+    df_bronze.writeStream
+    .format("delta")
+    .outputMode("append")
+    .option("checkpointLocation", CHECKPOINT_PATH)
+    .trigger(availableNow=True)
+    .toTable(TARGET_TABLE)
+)
+
+bronze_stream.awaitTermination()
